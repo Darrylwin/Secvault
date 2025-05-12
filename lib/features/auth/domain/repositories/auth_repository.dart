12718ff078ea050ff9db1e0309b5e651508a734d
Darrylwin <logossoudarryl20@gmail.com>
2025-05-12 11:50:12 +0000

    update AuthRepository to include login, register, logout, and getCurrentUser methods

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,5 +1,13 @@
+import 'package:dartz/dartz.dart';
 import 'package:secvault/features/auth/domain/entities/user.dart';
+import 'package:secvault/features/auth/presentation/bloc/auth_state.dart';
 
 abstract class AuthRepository {
- Future<User> getUser();
+  Future<Either<AuthFailure, User>> login(String email, String password);
+
+  Future<Either<AuthFailure, User>> register(String email, String password);
+
+  Future<Either<AuthFailure, void>> logout();
+
+  Future<Either<AuthFailure, User?>> getCurrentUser();
 }
