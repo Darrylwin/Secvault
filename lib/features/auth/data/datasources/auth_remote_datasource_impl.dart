57cec2622e08ba1f028c27904c1be81e1d0c153e
Darrylwin <logossoudarryl20@gmail.com>
2025-05-13 10:02:19 +0000

    feat: implement AuthRemoteDatasourceImpl for authentication operations

---
+++ lib/features/auth/data/datasources/auth_remote_datasource_impl.dart
@@ -0,0 +1,81 @@
+import 'package:secvault/features/auth/data/models/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'auth_remote_datasource.dart';
+
+class AuthRemoteDatasourceImpl implements AuthRemoteDatasource {
+  final FirebaseAuth _firebaseAuth;
+
+  const AuthRemoteDatasourceImpl(this._firebaseAuth);
+
+  @override
+  Future<UserModel> login(String? email, String password) async {
+    try {
+      final UserCredential userCredential =
+          await _firebaseAuth.signInWithEmailAndPassword(
+        email: email!,
+        password: password,
+      );
+
+      final user = userCredential.user!;
+
+      return UserModel(
+        uid: user.uid,
+        email: user.email,
+        name: user.displayName,
+      );
+    } catch (e) {
+      print("Logging error: $e");
+      rethrow;
+    }
+  }
+
+  @override
+  Future<UserModel> register(String? email, String password) async {
+    try {
+      final UserCredential userCredential =
+          await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email!,
+        password: password,
+      );
+
+      final user = userCredential.user!;
+
+      return UserModel(
+        email: user.email!,
+        name: user.displayName,
+        uid: user.uid,
+      );
+    } catch (e) {
+      print("Registration error: $e");
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> logout() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (error) {
+      print("Logout error: $error");
+      rethrow;
+    }
+  }
+
+  @override
+  Future<UserModel?> getCurrentUser() async {
+    try {
+      final User? user = await _firebaseAuth.currentUser;
+
+      if (user != null) {
+        return UserModel(
+          email: user.email,
+          name: user.displayName,
+          uid: user.uid,
+        );
+      }
+    } catch (error) {
+      print("Get current user error: $error");
+      rethrow;
+    }
+  }
+}
