017a429fe1b689097b877af652f542d6a843f0ce
Darrylwin <logossoudarryl20@gmail.com>
2025-05-13 21:08:44 +0000

    feat: enhance AuthBloc with logout, registration, and current user retrieval functionality

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,13 +1,25 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:secvault/features/auth/domain/usecases/get_current_user.dart';
+import 'package:secvault/features/auth/domain/usecases/register.dart';
+import '../../domain/entities/user.dart';
 import '../../domain/usecases/login.dart';
+import '../../domain/usecases/logout.dart';
 import 'auth_event.dart';
 import 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final Login loginUsecase;
+  final Logout logoutUsecase;
+  final Register registerUsecase;
+  final GetCurrentUser getCurrentUserUsecase;
 
-  AuthBloc({required this.loginUsecase}) : super(AuthInitial()) {
+  AuthBloc(this.logoutUsecase, this.registerUsecase, this.getCurrentUserUsecase,
+      {required this.loginUsecase})
+      : super(AuthInitial()) {
     on<LoginRequested>(_onLoginRequested);
+    on<LogoutRequested>(_onLogoutRequested);
+    on<RegisterRequested>(_onRegisterRequested);
+    on<CheckAuthRequested>(_onCheckAuthRequested);
   }
 
   Future<void> _onLoginRequested(
@@ -18,8 +30,85 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       final result =
           await loginUsecase(email: event.email, password: event.password);
 
-      result.fold((failure) => emit(AuthFailure(failure.message)),
-          (success) => emit(AuthSuccess()));
+      result.fold(
+        (failure) => emit(AuthFailure(failure.message)),
+        (success) => emit(
+          AuthSuccess(
+            User(
+              email: success.email,
+              name: success.name,
+              uid: success.uid,
+            ),
+          ),
+        ),
+      );
+    } catch (e) {
+      emit(AuthFailure('$e'));
+    }
+  }
+
+  Future<void> _onLogoutRequested(
+      LogoutRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+
+    try {
+      final result = await logoutUsecase();
+
+      result.fold(
+        (failure) => emit(AuthFailure(failure.message)),
+        (success) => emit(Unauthenticated()),
+      );
+    } catch (e) {
+      emit(AuthFailure('$e'));
+    }
+  }
+
+  Future<void> _onRegisterRequested(
+      RegisterRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+
+    try {
+      final result = await registerUsecase(
+        email: event.email,
+        password: event.password,
+      );
+
+      result.fold(
+        (failure) => emit(AuthFailure(failure.message)),
+        (success) => emit(
+          AuthSuccess(
+            User(
+              email: success.email,
+              name: success.name,
+              uid: success.uid,
+            ),
+          ),
+        ),
+      );
+    } catch (e) {
+      emit(AuthFailure('$e'));
+    }
+  }
+
+  Future<void> _onCheckAuthRequested(
+      CheckAuthRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+
+    try {
+      final result = await getCurrentUserUsecase();
+
+      result.fold(
+        (failure) => emit(Unauthenticated()),
+        (user) => emit(
+          AuthSuccess(
+            User(
+              email: user?.email,
+              name: user?.name,
+              uid: user!.uid,
+            ),
+          ),
+        ),
+      );
     } catch (e) {
       emit(AuthFailure('$e'));
     }
