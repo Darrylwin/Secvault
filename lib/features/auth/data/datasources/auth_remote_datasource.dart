f4d9c349f34675eef5986ce5fcb6cb4b41ba56a2
Darrylwin <logossoudarryl20@gmail.com>
2025-05-13 10:01:46 +0000

    feat: update AuthRemoteDatasource to allow nullable email in login and register methods

--- lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -1,9 +1,9 @@
 import 'package:secvault/features/auth/data/models/user_model.dart';
 
 abstract class AuthRemoteDatasource {
-  Future<UserModel> login(String email, String password);
+  Future<UserModel> login(String? email, String password);
 
-  Future<UserModel> register(String email, String password);
+  Future<UserModel> register(String? email, String password);
 
   Future<void> logout();
 
