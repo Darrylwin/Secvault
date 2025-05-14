387b87d7c62a06e23de554d1be48258cdc3c5094
Darrylwin <logossoudarryl20@gmail.com>
2025-05-14 10:12:51 +0000

    feat: add MyTextField widget for customizable text input

---
+++ lib/features/auth/presentation/widgets/my_text_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  MyTextField({
+    super.key,
+    required this.obscureText,
+    required this.hintText,
+    required this.controller,
+    required this.icon,
+  });
+
+  final TextEditingController controller;
+  bool obscureText;
+  final String hintText;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        hintText: hintText,
+        prefixIcon: Icon(icon),
+        filled: true,
+        fillColor: const Color(0xffefefef),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
