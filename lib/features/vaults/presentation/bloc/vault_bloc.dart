e9271fe2958e719a60395714dc73bb11342c257f
Darrylwin <logossoudarryl20@gmail.com>
2025-05-29 23:02:40 +0000

    add _onDeleteVault and _onLoadVaults events handing

--- lib/features/vaults/presentation/bloc/vault_bloc.dart
+++ lib/features/vaults/presentation/bloc/vault_bloc.dart
@@ -1,14 +1,15 @@
-import 'package:bloc/bloc.dart';
-import 'package:secvault/features/vaults/domain/errors/vault_failure.dart';
-import 'package:secvault/features/vaults/domain/usecases/get_all_vaults.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:secvault/features/vaults/domain/usecases/create_vault_usecase.dart';
+import 'package:secvault/features/vaults/domain/usecases/delete_vault_usecase.dart';
+import 'package:secvault/features/vaults/domain/usecases/get_all_vaults_usecase.dart';
 
 import 'vault_event.dart';
 import 'vault_state.dart';
 
 class VaultBloc extends Bloc<VaultEvent, VaultState> {
-  final GetAllVaults getAllVaults;
-  final CreateVault createVault;
-  final DeleteVault deleteVault;
+  final GetAllVaultsUsecase getAllVaults;
+  final CreateVaultUsecase createVault;
+  final DeleteVaultUsecase deleteVault;
 
   VaultBloc({
     required this.deleteVault,
@@ -16,14 +17,38 @@ class VaultBloc extends Bloc<VaultEvent, VaultState> {
     required this.getAllVaults,
   }) : super(VaultInitial()) {
     on<LoadVaults>(_onLoadVaults);
+    on<CreateVault>(_onCreateVault);
+    on<DeleteVault>(_onDeleteVault);
   }
 
-  void _onLoadVaults(VaultEvent event, Emitter<VaultState> emitter) async {
+  Future<void> _onLoadVaults(VaultEvent event, Emitter<VaultState> emit) async {
     emit(VaultLoading());
     final result = await getAllVaults();
     result.fold(
       (failure) => emit(VaultError(failure.message)),
       (vaults) => emit(VaultLoaded(vaults)),
+    );
+  }
+
+  Future<void> _onCreateVault(
+    CreateVault event,
+    Emitter<VaultState> emit,
+  ) async {
+    emit(VaultLoading());
+    final result = await createVault(name: event.name);
+    result.fold(
+      (failure) => emit(VaultError(failure.message)),
+      (success) => add(LoadVaults()),
+    );
+  }
+
+  Future<void> _onDeleteVault(
+      DeleteVault event, Emitter<VaultState> emit) async {
+    emit(VaultLoading());
+    final result = await deleteVault(vaultId: event.vaultId);
+    result.fold(
+      (failure) => emit(VaultError(failure.message)),
+      (success) => add(LoadVaults()),
     );
   }
 }
