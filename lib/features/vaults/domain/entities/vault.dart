653ebdf05910f54b6ae97faaf7c1f2f47f6e2d05
Darrylwin <logossoudarryl20@gmail.com>
2025-05-29 16:44:55 +0000

    feat: create Vault class with id, name, createdAt, and optional updatedAt fields

---
+++ lib/features/vaults/domain/entities/vault.dart
@@ -0,0 +1,13 @@
+class Vault {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+  final DateTime? updatedAt;
+
+  Vault({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+}
