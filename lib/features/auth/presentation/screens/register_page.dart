9c91ec2c19a8e13d6a21693c31161ca2163abd1c
Darrylwin <logossoudarryl20@gmail.com>
2025-05-29 14:25:36 +0000

    feat: enhance RegisterPage layout with scrollable view and improved UI structure

--- lib/features/auth/presentation/screens/register_page.dart
+++ lib/features/auth/presentation/screens/register_page.dart
@@ -54,6 +54,7 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFAFAFAFF),
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: BlocListener<AuthBloc, AuthState>(
           listener: (context, state) {
@@ -67,165 +68,177 @@ class _RegisterPageState extends State<RegisterPage> {
               );
             }
           },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 25),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Center(
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height -
+                    MediaQuery.of(context).padding.top -
+                    MediaQuery.of(context).padding.bottom,
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25.0, vertical: 25),
+                child: IntrinsicHeight(
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      SvgPicture.asset(
-                        "assets/images/undraw_adventure_map.svg",
-                        width: 100,
-                        height: 100,
-                      ),
-                      const Text(
-                        "Get started",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Text(
-                        "Let's create a free account",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Column(
-                  children: [
-                    MyTextField(
-                      obscureText: false,
-                      hintText: "Full name",
-                      controller: nameController,
-                      icon: Icons.person,
-                    ),
-                    const SizedBox(height: 15),
-                    MyTextField(
-                      obscureText: false,
-                      hintText: "Valid email",
-                      controller: emailController,
-                      icon: Icons.mail_outline_rounded,
-                    ),
-                    const SizedBox(height: 15),
-                    MyTextField(
-                      obscureText: true,
-                      hintText: "Strong password",
-                      controller: passwordController,
-                      icon: Icons.lock_outline,
-                    ),
-                    const SizedBox(height: 15),
-                    MyTextField(
-                      obscureText: true,
-                      hintText: "Confirm password",
-                      controller: confirmPasswordController,
-                      icon: Icons.lock_outline,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Checkbox(
-                          value: agreed,
-                          onChanged: agreeTermsAndConditions,
-                          checkColor: const Color(0xFFFD3951),
-                          activeColor: Colors.transparent,
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            children: [
-                              TextSpan(
-                                text: "Do you agree with our ",
-                                style: TextStyle(
-                                  color: Colors.grey[600],
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w300,
-                                ),
-                              ),
-                              const TextSpan(
-                                text: "Terms and Conditions ?",
-                                style: TextStyle(
-                                  color: Color(0xFFFD3951),
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w300,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    GestureDetector(
-                      onTap: _onregisterPressed,
-                      child: Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: const Color(0xFFFD3951),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                      Center(
+                        child: Column(
                           children: [
-                            Text(
-                              "Next",
+                            SvgPicture.asset(
+                              "assets/images/undraw_adventure_map.svg",
+                              width: 100,
+                              height: 100,
+                            ),
+                            const Text(
+                              "Get started",
                               style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
+                                color: Colors.black,
+                                fontSize: 30,
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                            SizedBox(width: 10),
-                            Icon(
-                              Icons.arrow_forward_ios_outlined,
-                              color: Colors.white,
-                              size: 15,
+                            const Text(
+                              "Let's create a free account",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w300,
+                              ),
                             ),
                           ],
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 15),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          "Already a member? ",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 13,
-                            fontWeight: FontWeight.w300,
+                      Column(
+                        children: [
+                          MyTextField(
+                            obscureText: false,
+                            hintText: "Full name",
+                            controller: nameController,
+                            icon: Icons.person,
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).pushReplacementNamed(
-                                '/login'); // Navigate to login page
-                          },
-                          child: const Text(
-                            "Log in",
-                            style: TextStyle(
-                              color: Color(0xFFFD3951),
-                              fontSize: 13,
-                              fontWeight: FontWeight.w500,
+                          const SizedBox(height: 15),
+                          MyTextField(
+                            obscureText: false,
+                            hintText: "Valid email",
+                            controller: emailController,
+                            icon: Icons.mail_outline_rounded,
+                          ),
+                          const SizedBox(height: 15),
+                          MyTextField(
+                            obscureText: true,
+                            hintText: "Strong password",
+                            controller: passwordController,
+                            icon: Icons.lock_outline,
+                          ),
+                          const SizedBox(height: 15),
+                          MyTextField(
+                            obscureText: true,
+                            hintText: "Confirm password",
+                            controller: confirmPasswordController,
+                            icon: Icons.lock_outline,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Checkbox(
+                                value: agreed,
+                                onChanged: agreeTermsAndConditions,
+                                checkColor: const Color(0xFFFD3951),
+                                activeColor: Colors.transparent,
+                              ),
+                              RichText(
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: "Do you agree with our ",
+                                      style: TextStyle(
+                                        color: Colors.grey[600],
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.w300,
+                                      ),
+                                    ),
+                                    const TextSpan(
+                                      text: "Terms and Conditions ?",
+                                      style: TextStyle(
+                                        color: Color(0xFFFD3951),
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.w300,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          GestureDetector(
+                            onTap: _onregisterPressed,
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: const Color(0xFFFD3951),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: const Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Next",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Icon(
+                                    Icons.arrow_forward_ios_outlined,
+                                    color: Colors.white,
+                                    size: 15,
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                          const SizedBox(height: 15),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                "Already a member? ",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context).pushReplacementNamed(
+                                      '/login'); // Navigate to login page
+                                },
+                                child: const Text(
+                                  "Log in",
+                                  style: TextStyle(
+                                    color: Color(0xFFFD3951),
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ],
+              ),
             ),
           ),
         ),
