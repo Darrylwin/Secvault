0b44ee4c1b3562456f89adae80b915667f9aea7b
Darrylwin <logossoudarryl20@gmail.com>
2025-05-29 16:45:24 +0000

    remove required updatedAt field from Vault constructor

--- lib/features/vaults/domain/entities/vault.dart
+++ lib/features/vaults/domain/entities/vault.dart
@@ -2,12 +2,10 @@ class Vault {
   final String id;
   final String name;
   final DateTime createdAt;
-  final DateTime? updatedAt;
 
   Vault({
     required this.id,
     required this.name,
     required this.createdAt,
-    required this.updatedAt,
   });
 }
