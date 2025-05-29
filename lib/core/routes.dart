509906ef80bcba96da0f1593967636ccacadc2eb
Darrylwin <logossoudarryl20@gmail.com>
2025-05-29 13:40:40 +0000

    feat: integrate authentication with Bloc in LoginPage and add home route

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:secvault/features/vaults/presentation/screens/home_page.dart';
 import '../features/auth/presentation/screens/login_page.dart';
 import '../features/auth/presentation/screens/register_page.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
   '/login': (context) => const LoginPage(),
   '/register': (context) => const RegisterPage(),
+  '/home': (context) => const HomePage(),
 };
