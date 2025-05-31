76af01ce99d492a92ecb944958e67fca2302c549
Darrylwin <logossoudarryl20@gmail.com>
2025-05-31 03:44:28 +0000

    defined light theme

---
+++ lib/core/themes/light_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  colorScheme: const ColorScheme.light(
+    brightness: Brightness.light,
+    primary: Colors.black,
+  ),
+  useMaterial3: true,
+  scaffoldBackgroundColor: Colors.white,
+);
