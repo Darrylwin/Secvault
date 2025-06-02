238895da4374511900687ee6ec248127e4060bdb
Darrylwin <logossoudarryl20@gmail.com>
2025-06-02 17:51:54 +0000

    feat: refactor VaultAccessBloc to improve variable naming and add list members functionality

--- lib/features/access_control/presentation/bloc/vault_access_bloc.dart
+++ lib/features/access_control/presentation/bloc/vault_access_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:secvault/features/access_control/domain/usecases/invite_user_to_vault_useacse.dart';
 import 'package:secvault/features/access_control/domain/usecases/list_vault_members_usecase.dart';
@@ -6,14 +8,14 @@ import 'package:secvault/features/access_control/presentation/bloc/vault_access_
 import 'package:secvault/features/access_control/presentation/bloc/vault_access_state.dart';
 
 class VaultAccessBloc extends Bloc<VaultAccessEvent, VaultAccessState> {
-  final InviteUserToVaultUseacse inviteUserToVaultEvent;
-  final ListVaultMembersUsecase listVaultMembersEvent;
-  final RevokeUserAccessUsecase revokeUserAccessEvent;
+  final InviteUserToVaultUseacse inviteUserToVaultUseacse;
+  final ListVaultMembersUsecase listVaultMembersUseacse;
+  final RevokeUserAccessUsecase revokeUserAccessUseacse;
 
   VaultAccessBloc({
-    required this.revokeUserAccessEvent,
-    required this.listVaultMembersEvent,
-    required this.inviteUserToVaultEvent,
+    required this.revokeUserAccessUseacse,
+    required this.listVaultMembersUseacse,
+    required this.inviteUserToVaultUseacse,
   }) : super(VaultAccessInitial()) {
     on<InviteUserToVaultEvent>(_onInviteUser);
     on<ListVaultMembersEvent>(_onListMembers);
@@ -23,7 +25,7 @@ class VaultAccessBloc extends Bloc<VaultAccessEvent, VaultAccessState> {
   Future<void> _onInviteUser(
       InviteUserToVaultEvent event, Emitter<VaultAccessState> emit) async {
     emit(VaultAccessLoading());
-    final result = await inviteUserToVaultEvent(
+    final result = await inviteUserToVaultUseacse(
       vaultId: event.vaultId,
       userEmail: event.userEmail,
       role: event.role,
@@ -32,6 +34,16 @@ class VaultAccessBloc extends Bloc<VaultAccessEvent, VaultAccessState> {
     result.fold(
       (error) => emit(VaultAccessError('$error')),
       (success) => emit(VaultAccessSuccess("User successfully inited")),
+    );
+  }
+
+  Future<void> _onListMembers(
+      ListVaultMembersEvent event, Emitter<VaultAccessState> emit) async {
+    emit(VaultAccessLoading());
+    final members = await listVaultMembersUseacse(vaultId: event.vaultId);
+    members.fold(
+      (error) => emit(VaultAccessError(error.toString())),
+      (successs) => emit(VaultMembersLoaded(successs)),
     );
   }
 }
