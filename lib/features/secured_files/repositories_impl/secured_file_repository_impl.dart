fcdd91b3521429d0ebd628785d9c201ff398f8b6
Darrylwin <logossoudarryl20@gmail.com>
2025-06-03 08:09:28 +0000

    implement uploadSecuredFile method in SecuredFileRepositoryImpl

---
+++ lib/features/secured_files/repositories_impl/secured_file_repository_impl.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:secvault/features/secured_files/data/datasources/remote/secured_file_remote_datasource.dart';
+import 'package:secvault/features/secured_files/domain/errors/secured_file_failure.dart';
+import 'package:secvault/features/secured_files/domain/repositories/secure_file_repository.dart';
+
+class SecuredFileRepositoryImpl implements SecuredFileRepository {
+  final SecuredFileRemoteDatasource securedFileRemoteDatasource;
+
+  SecuredFileRepositoryImpl(this.securedFileRemoteDatasource);
+
+  @override
+  Future<Either<SecuredFileFailure, void>> uploadSecuredFile(
+      {required String vaultId,
+      required String fileName,
+      required List<int> rawData}) async {
+    try {
+      await securedFileRemoteDatasource.uploadSecuredFile(
+        fileName: fileName,
+        vaultId: vaultId,
+        rawData: rawData,
+      );
+      return const Right(null);
+    } catch (e) {
+      return Left(SecuredFileFailure("Error uploading file: $e"));
+    }
+  }
+}
