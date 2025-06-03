2d437e3697ba91dfd55022e7f8547168fd927fef
Darrylwin <logossoudarryl20@gmail.com>
2025-06-03 08:48:54 +0000

    implement SecuredFileBloc, events, and states

---
+++ lib/features/secured_files/presentation/bloc/secured_file_state.dart
@@ -0,0 +1,27 @@
+import 'package:secvault/features/secured_files/domain/entities/secured_file.dart';
+
+abstract class SecuredFileState {}
+
+class SecuredFileInitial extends SecuredFileState {}
+
+class SecuredFileLoading extends SecuredFileState {}
+
+class SecuredFileListSuccess extends SecuredFileState {
+  final List<SecuredFile> files;
+
+  SecuredFileListSuccess(this.files);
+}
+
+class SecuredFileDownloadSuccess extends SecuredFileState {
+  final SecuredFile file;
+
+  SecuredFileDownloadSuccess(this.file);
+}
+
+class SecuredFileSuccess extends SecuredFileState {}
+
+class SecuredFileError extends SecuredFileState {
+  final String message;
+
+  SecuredFileError(this.message);
+}
