eb4d5fd7d641f69d2505f96ac06e09fb1ecd6e3e
Darrylwin <logossoudarryl20@gmail.com>
2025-06-03 08:13:15 +0000

    implement deleteSecuredFile method in SecuredFileRepositoryImpl

--- lib/features/secured_files/repositories_impl/secured_file_repository_impl.dart
+++ lib/features/secured_files/repositories_impl/secured_file_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:secvault/features/secured_files/data/datasources/remote/secured_file_remote_datasource.dart';
+import 'package:secvault/features/secured_files/domain/entities/secured_file.dart';
 import 'package:secvault/features/secured_files/domain/errors/secured_file_failure.dart';
 import 'package:secvault/features/secured_files/domain/repositories/secure_file_repository.dart';
 
@@ -22,6 +23,20 @@ class SecuredFileRepositoryImpl implements SecuredFileRepository {
       return const Right(null);
     } catch (e) {
       return Left(SecuredFileFailure("Error uploading file: $e"));
+    }
+  }
+
+  @override
+  Future<Either<SecuredFileFailure, void>> deleteSecuredFile(
+      {required String vaultId, required String fileId}) async {
+    try {
+      await securedFileRemoteDatasource.deleteSecuredFile(
+        fileId: fileId,
+        vaultId: vaultId,
+      );
+      return const Right(null);
+    } catch (e) {
+      return Left(SecuredFileFailure("Error deleting file: $e"));
     }
   }
 }
