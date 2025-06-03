c0fb5eaf8dd7c2c6c3c786438ecf2fce0f11304c
Darrylwin <logossoudarryl20@gmail.com>
2025-06-03 08:49:35 +0000

    implement SecuredFileBloc, events, and states

---
+++ lib/features/secured_files/presentation/bloc/secured_file_event.dart
@@ -0,0 +1,39 @@
+abstract class SecuredFileEvent {}
+
+class UploadSecuredFileEvent extends SecuredFileEvent {
+  final String vaultId;
+  final String fileName;
+  final List<int> rawData;
+
+  UploadSecuredFileEvent({
+    required this.vaultId,
+    required this.fileName,
+    required this.rawData,
+  });
+}
+
+class DeleteSecuredFileEvent extends SecuredFileEvent {
+  final String vaultId;
+  final String fileId;
+
+  DeleteSecuredFileEvent({
+    required this.vaultId,
+    required this.fileId,
+  });
+}
+
+class ListSecuredFilesEvent extends SecuredFileEvent {
+  final String vaultId;
+
+  ListSecuredFilesEvent(this.vaultId);
+}
+
+class DownloadSecuredFileEvent extends SecuredFileEvent {
+  final String vaultId;
+  final String fileId;
+
+  DownloadSecuredFileEvent({
+    required this.vaultId,
+    required this.fileId,
+  });
+}
