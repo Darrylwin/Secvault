8ba4d88c0277836b5523d7167dc6dd87c1e33811
Darrylwin <logossoudarryl20@gmail.com>
2025-06-04 19:01:04 +0000

    feat: add GetAccessibleVaults use case and update VaultRepository interface

--- lib/features/vaults/domain/repositories/vault_repository.dart
+++ lib/features/vaults/domain/repositories/vault_repository.dart
@@ -9,4 +9,6 @@ abstract class VaultRepository {
   Future<Either<VaultFailure, void>> deleteVault(String vaultId);
 
   Future<Either<VaultFailure, List<Vault>>> getAllVaults();
+
+  Future<Either<VaultFailure, List<Vault>>> getAccessibleVaults(String userId);
 }
